80c44144bb45ebcce88e2d7c506f1ec57b10f209
gmp26 <gmp26@cam.ac.uk>
2020-01-20 10:42:49 +0000

    added iain notation and an HHG@G puzzle

--- src/arrows/core.clj
+++ src/arrows/core.clj
@@ -24,6 +24,17 @@
    :w  [-1 0]
    :nw [-1 -1]})
 
+(def label
+  "map labels coordinates according to Iain' Bratchie's notation"
+  (zipmap (for [i (range 4)
+                j (range 4)] [j i]) (map inc (range))))
+
+(defn iain-notation
+  "Convert 2 counter coordinates to an Iain position"
+  [[c0 c1]]
+  (list (label c0) (label c1)))
+
+
 (defn arrow-under
   "The direction arrow under grid g at position [x y]"
   [g x y]
@@ -48,13 +59,22 @@
     position
     (follow-path (move position (first path)) (rest path))))
 
-(defn print-path [from path]
+(defn print-path [from path translate]
   (let [step (first path)]
     (when step
-      (println from "->" (move from step) "using"
+      (println (translate (move from step)) "using"
         (apply arrow-under (- 1 step) (from (- 1 step)))
         (if (zero? step) "right" "left"))
-      (print-path (move from step) (rest path)))))
+      (print-path (move from step) (rest path) translate))))
+
+(defn as-number [path]
+  (reduce (fn [n b] (+ (* 2 n) b)) 0 path))
+
+(comment
+  (as-number [1 0 0])
+  )
+
+(def ^:dynamic *notation*  identity)
 
 (defn search-for-solution [start finish paths visited]
   (let [path (first paths)
@@ -63,15 +83,20 @@
                    (let [max-grid-index (dec (count (get-in grid [0 0])))]
                      (and (<= 0 x max-grid-index) (<= 0 y max-grid-index))))
         invalid? (fn [[c0 c1]]
-                     (not (and (on-grid? c0) (on-grid? c1))))]
+                   (not (and (on-grid? c0) (on-grid? c1))))]
     (if path
       (let [position (follow-path start path)]
         (cond
           ; are we there yet?
           (= position finish)
-          (print-path start path)
+          (do
+            (println "Using 0 for move left counter, and 1 for a right counter,")
+            (println "a length " (count path) "solution is" path)
+            (println "which, interpreted as a binary number, means the solution is "
+              (as-number path))
+            (print-path start path *notation*))
 
-          ; have we been here before? or ae we off-grid?
+          ; have we been here before? or are we off-grid?
           (or (visited position) (invalid? position))
           (search-for-solution start finish remaining-paths visited) ;continue searching
 
@@ -90,7 +115,8 @@
   "Look for a path from the start position to the end position. See examples below.
   Kick things off by adding paths [0] and [1] to the search queue, having visited nowhere yet."
   [start finish]
-  (search-for-solution start finish [[0] [1]] {}))
+  (search-for-solution start finish [[0] [1]] {})
+  (println))
 
 (comment
   ;
@@ -102,7 +128,13 @@
     [[0 0] [0 0]]
     [[3 3] [3 3]]
     )
-  ;=>  The solution is  [1 0 0 1 1 0 0 0 1 1 0 0 1 1 0 1 0 0 1 0 1 0 0 0 1 1 0 1 0 0 1 1 0 0 0 1 1 1 0 1 0 1 1 0 1 1 0 0 1 0 0 0]
+
+  "Or using Iain's notation"
+  (with-redefs [*notation* iain-notation]
+    (find-route
+      [[0 0] [0 0]]
+      [[3 3] [3 3]]
+      ))
 
   (find-route
     [[3 3] [3 3]]
@@ -151,4 +183,16 @@
   (find-route [[0 0] [0 0]] [[1 0] [0 1]])
   (find-route [[0 0] [0 0]] [[0 0] [1 1]])
   (find-route [[0 0] [0 0]] [[1 1] [0 0]])
+
+  ; Or using Iain's notation:
+  (with-redefs [*notation* iain-notation]
+    (find-route [[0 0] [0 0]] [[1 1] [1 1]])
+    (find-route [[1 1] [1 1]] [[0 0] [0 0]])
+    (find-route [[0 0] [0 0]] [[0 1] [1 0]])
+    (find-route [[0 0] [0 0]] [[1 0] [0 1]])
+    (find-route [[0 0] [0 0]] [[0 0] [1 1]])
+    (find-route [[0 0] [0 0]] [[1 1] [0 0]]))
+
+
+  ; Also for HHG2G fans, can you find a puzzle that has the numerical solution 42?
   )
